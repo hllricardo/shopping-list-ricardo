9de50cbb7d9537d912a62e8185833cbba3d82038
hllricardo <hernandezlloretricardo@.gmail.com>
2025-09-13 10:56:10 +0200

    feat: add removeItem function to delete products

--- ShoppingList.js
+++ ShoppingList.js
@@ -1,7 +1,14 @@
+// array utilizada como lista
 const shoppingList = [
     {item: "", cantidad: 0, comprado: false}
 ];
 
+// funcion para añadir objetos al array creado
 function addItem(item, quantity){
-    shoppingList.push({item: item, cantidad: quantity});
+    shoppingList.push({item: item, cantidad: quantity, comprado: false});
+}
+
+// funcion para eliminar un objeto de la lista mediante su posicion
+function removeItem(id){
+    shoppingList.splice(id);
 }
